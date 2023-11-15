d38e95d8b3d4dac7797b0a061951c0bf5e5c1a25
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-14 21:27:55 -0500

    Update doomsday.js

--- doomsday.js
+++ doomsday.js
@@ -1,55 +1,45 @@
 const readline = require('readline');
 
-var dd = new Date("January 3, 2018"); //can be any doomsday
+// Define a reference doomsday date
+const referenceDoomsday = new Date("January 3, 2018");
 
-function dateify(input){
-	var date = new Date(input + ", 2018");
-	return date;
+// Function to convert input to a date
+function parseInputToDate(input) {
+    return new Date(input + ", 2018");
 }
 
+// Function to calculate the difference in days between two dates
+function calculateDateDifference(dateA, dateB) {
+    const dateAms = dateA.getTime();
+    const dateBms = dateB.getTime();
 
-function dateDifference(dateA, dateB){
-	var dateAms = dateA.getTime();
-	var dateBms = dateB.getTime();
+    // Calculate the absolute difference in milliseconds
+    const absoluteDiffMs = Math.abs(dateBms - dateAms);
 
-	var diffA = dateBms - dateAms;
-	var diffB = dateAms - dateBms;
+    // Convert milliseconds to days and round
+    const days = Math.round(absoluteDiffMs / (1000 * 60 * 60 * 24));
 
-	var days = Math.round(diffB/(1000*60*60*24));
-
-	return days;
+    return days;
 }
 
-function mod(n,m){
-	return ((n % m) + m) % m;
+// Function to find the day of the week based on the number of days
+function findDayOfWeek(days) {
+    const daysOfWeek = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
+    const remainder = ((days % 7) + 7) % 7; // Ensure positive remainder
+
+    return daysOfWeek[remainder];
 }
 
-function dayfinder(days){
-	remainder = mod(days,7);
-
-	if(remainder === 0) {
-		return "Wedneday";
-	} else if (remainder == 1){
-		return "Thursday";
-	} else if (remainder == 2){
-                return "Friday";
-        } else if (remainder == 3){
-                return "Saturday";
-        } else if (remainder == 4){
-                return "Sunday";
-        } else if (remainder == 5){
-                return "Monday";
-        } else if (remainder == 6){
-                return "Tuesday";
-        } 
-
-}
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 rl.question('Enter a month and a day: \n', (answer) => {
-	console.log(dayfinder(dateDifference(dateify(answer), dd)));
-  rl.close();
+    const inputDate = parseInputToDate(answer);
+    const daysDifference = calculateDateDifference(inputDate, referenceDoomsday);
+    const dayOfWeek = findDayOfWeek(daysDifference);
+
+    console.log(`The day of the week for ${inputDate.toDateString()} is ${dayOfWeek}.`);
+    rl.close();
 });

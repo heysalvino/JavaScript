84d10267f6089b19c8c6c861d0045cc4037f23d7
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-14 21:29:31 -0500

    Update primes.js

--- primes.js
+++ primes.js
@@ -6,7 +6,41 @@ function testIfPrime(n, primelist) {
 			return false; 
         	}
 
-        	i++;
+        	i++;function isPrime(number, primes) {
+    const sqrtNumber = Math.sqrt(number);
+
+    for (let i = 0; i < primes.length; i++) {
+        if (primes[i] > sqrtNumber) {
+            break;
+        }
+        if (number % primes[i] === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function findNthPrime(n) {
+    if (n <= 0) {
+        return undefined;
+    }
+
+    const primes = [2];
+    let currentNumber = 3;
+
+    while (primes.length < n) {
+        if (isPrime(currentNumber, primes)) {
+            primes.push(currentNumber);
+        }
+        currentNumber += 2; // Skip even numbers
+    }
+
+    return primes[n - 1];
+}
+
+console.log(findNthPrime(10001));
+
         	j = primelist[i];
 	}
 	return true;

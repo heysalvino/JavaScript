3bad640633e9f5f3390aba6706e247254ba2a274
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-14 21:27:12 -0500

    Update chalk.js

--- chalk.js
+++ chalk.js
@@ -1,17 +1,21 @@
 const chalk = require('chalk');
 
-function rainbow(string){
-    var output = ""
-    var colors = ['red', 'orange', 'yellow', 'green', 'navy', 'purple'];
-    var j = 0;
+/**
+ * Apply a rainbow effect to a string using Chalk.
+ * @param {string} string - The input string to colorize.
+ * @returns {string} - The rainbow-colored string.
+ */
+function rainbow(string) {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    let rainbowString = '';
 
-    for(let i = 0; i < string.length; i++) {
-      output += chalk.keyword(colors[j])(string[i]);
-      j++;
-      j = j % 6;
+    for (let i = 0; i < string.length; i++) {
+        const color = colors[i % colors.length]; // Cycle through the colors
+        rainbowString += chalk.keyword(color)(string[i]);
     }
-  return output;
+
+    return rainbowString;
 }
 
 console.log(rainbow("Taste the rainbow"));
-console.log(rainbow("Rainbow in the dark"))
+console.log(rainbow("Rainbow in the dark"));
